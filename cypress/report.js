45fac62b79a33722f0b1687d48897d971e6083fd
Carloseduardo1984 <carloseduardo1984@gmail.com>
2022-06-16 16:04:44 +0000

    Update report.js

--- cypress/report.js
+++ cypress/report.js
@@ -1,6 +1,42 @@
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "./cypress/cucumber-json/",
-  reportPath: "./cypress/cucumber-report/",
+    jsonDir: "./cypress/cucumber-json/",
+    reportPath: "./cypress/cucumber-report/",
+    metadata: {
+        // browser: {
+        //     name: "Chrome",
+        //     version: "Version 100.0.4896.127"
+        // },
+        // browser: {
+        //     name: "Firefox",
+        //     version: "99.0.1 (64-bit)"
+        // },
+        browser: {
+            name: "Chrome",
+            version: "^101"
+        },
+        device: "^Local Laptop i7",
+        platform: {
+            name: "windows",
+            version: "10"
+        }
+    },
+    customData: {
+        title: 'Run info',
+        data: [
+            { label: 'Project :', value: 'My project' },
+            { label: 'Release :', value: 'PI 2022' },
+            { label: 'Cycle :', value: '2022 Q3' },
+            { label: 'Environment :', value: 'QA' },
+            { label: 'Created on: ', value: '2022-06' },
+            { label: 'Tester :', value: 'CEO' }
+        ]
+    },
+    scenarioTimestamp: true,
+    displayDuration: true,
+    pageTitle: 'My Test Report',
+    reportName: 'Cypress BDD Test Report',
+    openReportInBrowser: true
+
 });
